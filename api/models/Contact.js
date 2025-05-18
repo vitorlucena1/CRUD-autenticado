ab561970f02aae01d25e79254747f2c45751c771
Vitor Lucena <104996449+vitorlucena1@users.noreply.github.com>
2025-05-18 17:01:03 +0000

    Conserto de erro

--- api/models/Contact.js
+++ api/models/Contact.js
@@ -28,8 +28,5 @@ const ContactSchema = new mongoose.Schema({
     }
 });
 
-// Índice composto para garantir que cada usuário tenha contatos com telefones únicos
-ContactSchema.index({ userId: 1, phone: 1 }, { unique: true });
-
 const Contact = mongoose.model("Contact", ContactSchema);
 export default Contact;
